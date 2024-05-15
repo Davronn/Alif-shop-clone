4d21cdfc77c19ba85125e6fcb71066385ad52b5e
Davronn <davronmahamadjonov8@gmai.com>
2024-05-15 15:56:34 +0500

    .

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -11,20 +11,47 @@ function Home() {
     fetchProducts();
   }, []);
 
+  const TruncateText = ({
+    text = "Text not provided",
+    maxLength,
+  }: {
+    maxLength: number;
+    text: string;
+  }) => {
+    const truncatedText =
+      text.length > maxLength ? text.slice(0, maxLength) + "..." : text;
+
+    return <span>{truncatedText}</span>;
+  };
+
   return (
     <div>
       {loading && <div>Loading...</div>}
       {error && <div>{error}</div>}
-      {products.map((product: ProductType , i) => (
-        <div key={product.id}>
-          <div>
-            {i+1} {product.title}$
+      <div className="flex flex-wrap justify-center gap-5">
+        {products.map((product: ProductType) => (
+          <div key={product.id}>
+            <div className="">
+              <div className="max-w-60 rounded overflow-hidden shadow-lg">
+                <img
+                  className="w-full"
+                  src={product.images[1]}
+                  alt={product.category}
+                />
+                <div className="px-6 py-4 font-bold text-xl mb-2">
+                  <TruncateText
+                    text={product.title}
+                    maxLength={20}
+                  />
+                  <p className="text-gray-700 text-base">
+                    <TruncateText text={product?.description} maxLength={8} />
+                  </p>
+                </div>
+              </div>
+            </div>
           </div>
-          <div>
-            <img className="w-40" src={product.images[1]} alt="" />
-          </div>
-        </div>
-      ))}
+        ))}
+      </div>
     </div>
   );
 }

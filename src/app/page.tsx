0806609a0a5fdda35bf15df77eacb91db6120884
Davronn <davronmahamadjonov8@gmai.com>
2024-05-15 16:40:30 +0500

    .

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -24,6 +24,8 @@ function Home() {
     return <span>{truncatedText}</span>;
   };
 
+  const defaultImg = "https://i.imgur.com/R2PN9Wq.jpeg"
+
   return (
     <div>
       {loading && <div>Loading...</div>}
@@ -32,21 +34,22 @@ function Home() {
         {products.map((product: ProductType) => (
           <div key={product.id}>
             <div className="">
-              <div className="max-w-60 rounded overflow-hidden shadow-lg">
+              <div className="max-w-60 h-[488px] rounded overflow-hidden shadow-lg">
                 <img
                   className="w-full"
-                  src={product.images[1]}
+                  src={product.images[1] ? product.images[1] : defaultImg }
                   alt={product.category}
                 />
                 <div className="px-6 py-4 font-bold text-xl mb-2">
-                  <TruncateText
-                    text={product.title}
-                    maxLength={20}
-                  />
-                  <p className="text-gray-700 text-base">
-                    <TruncateText text={product?.description} maxLength={8} />
+                  <TruncateText text={product.title} maxLength={20} />
+                  <p className="text-gray-700 text-base"> 
+                    <TruncateText text={product?.description} maxLength={24} />
                   </p>
+                  <p className="mt-3 text-gray-700 text-md">Price:  {product.price}$</p>
                 </div>
+                <button className=" bg-yellow-500 hover:bg-yellow-600 text-white font-bold py-2 px-3 rounded-full ml-4 mb-6">
+                  <span className="text-sm">Add to card</span>
+                </button>
               </div>
             </div>
           </div>
--- src/components/header/index.tsx
+++ src/components/header/index.tsx
@@ -1,9 +1,39 @@
-import React from 'react'
+import React from "react";
+import { BsCart2 } from "react-icons/bs";
+import { CiSearch } from "react-icons/ci";
+import { FaBars } from "react-icons/fa";
+import { MdFavoriteBorder } from "react-icons/md";
 
 function Header() {
   return (
-    <div>Header</div>
-  )
+    <div>
+      <header
+        className="flex items-center h-24 container w-full mx-auto justify-center gap-24"
+      >
+        <h1 className="text-2xl">Logo</h1>
+        <button className="flex">
+          <span>Pruducts</span>
+          <FaBars size={25} />
+        </button>
+        <div className="input">
+          <input type="text" placeholder="Search" />
+          <button>
+            <CiSearch />
+          </button>
+        </div>
+        <div className="cart">
+          <BsCart2 />
+          <span>Cart</span>
+        </div>
+        <div className="cart">
+          <MdFavoriteBorder />
+          <span>Favorites</span>
+        </div>
+        <button>Sign Up</button>
+        <p>РУС / UZB</p>
+      </header>
+    </div>
+  );
 }
 
-export default Header
+export default Header;

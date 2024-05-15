7802753814659cdc3ffb7a7f350893f0755ed841
Davronn <davronmahamadjonov8@gmai.com>
2024-05-15 15:12:25 +0500

    .

---
+++ src/store/product/productStore.ts
@@ -0,0 +1,32 @@
+import axios from "axios";
+import { create } from "zustand";
+
+const productStore = (set: any) => ({
+  loading: false,
+  products: [],
+  error: null,
+  fetchProducts: async () => {
+    try {
+      set(() => ({
+        loading: true,
+      }));
+      const res = await axios.get("https://api.escuelajs.co/api/v1/products");
+      const data = await res.data;
+      set(() => ({
+        products: data,
+      }));
+    } catch (err) {
+      set(() => ({
+        error: (err as Error).message,
+      }));
+    } finally {
+      set(() => ({
+        loading: false,
+      }));
+    }
+  },
+});
+
+const useProductStore = create(productStore);
+
+export default useProductStore;
